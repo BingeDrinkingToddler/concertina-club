5b812bf3159d63b153b5772e9695c74be1830770
BingeDrinkingToddler <lukewestlake10@gmail.com>
2019-01-22 16:05:52 +0000

    ha

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
+
 const app = require('./app');
 app.listen(process.env.PORT);
-=======
-const app = require('./app');
-app.listen(process.env.PORT);
->>>>>>> 93d1727d71b15a52d12b250d4498b1c2f63d5cc4
+
